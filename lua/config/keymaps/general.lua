d8e85566218fce12156cdb27e05014ef5fa44930
yashksaini-coder <yashksaini89@gmail.com>
2025-11-29 20:25:37 +0530

    feat(keymaps): Refactor keymap definitions for clarity and add autopairs keymap
    feat(plugins): Add nvim-autopairs and nvim-notify plugin configurations

--- lua/config/keymaps/general.lua
+++ lua/config/keymaps/general.lua
@@ -1,24 +1,46 @@
 -- ShowKeys Toggle keymap
-vim.keymap.set("n", "<leader>sk", "<cmd>ShowkeysToggle<CR>", { desc = "Toggle ShowKeys" })
+vim.keymap.set("n", "<leader>sk", "<cmd>ShowkeysToggle<CR>", {
+    desc = "Toggle ShowKeys"
+})
 
 -- Clear search highlights
-vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Clear search highlights" })
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>", {
+    desc = "Clear search highlights"
+})
 
 -- Better window navigation
-vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
-vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to lower window" })
-vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to upper window" })
-vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
+vim.keymap.set("n", "<C-h>", "<C-w>h", {
+    desc = "Move to left window"
+})
+vim.keymap.set("n", "<C-j>", "<C-w>j", {
+    desc = "Move to lower window"
+})
+vim.keymap.set("n", "<C-k>", "<C-w>k", {
+    desc = "Move to upper window"
+})
+vim.keymap.set("n", "<C-l>", "<C-w>l", {
+    desc = "Move to right window"
+})
 
 vim.keymap.set("n", "<leader>mp", function()
-            require("render-markdown").toggle()
-        end, {
-            desc = "Toggle Markdown Render"
-        })
+    require("render-markdown").toggle()
+end, {
+    desc = "Toggle Markdown Render"
+})
 
+-- Autopairs Keymap
+vim.keymap.set("v", "<C-/>", "<Plug>(comment_toggle_linewise_visual)", {
+    noremap = true,
+    silent = true,
+    desc = "Toggle comment"
+})
 
 -- Save file
-vim.keymap.set({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save file" })
+vim.keymap.set({"i", "x", "n", "s"}, "<C-s>", "<cmd>w<cr><esc>", {
+    desc = "Save file"
+})
 
 -- Quit                                                                                                             
-vim.keymap.set("n", "<leader>q", "<cmd>qa<cr>", { desc = "Quit all" })
+vim.keymap.set("n", "<leader>q", "<cmd>qa<cr>", {
+    desc = "Quit all"
+})
---
+++ lua/plugins/autopairs.lua
@@ -0,0 +1,9 @@
+return {
+  { "windwp/nvim-autopairs", event = "InsertEnter", config = true },
+  {
+    "numToStr/Comment.nvim",
+    config = function()
+        require('Comment').setup()
+        end
+  },
+}
