499b7b9d90da5f67474758a33a2a0a69278dcea2
yashksaini-coder <yashksaini89@gmail.com>
2025-12-04 01:13:21 +0530

    feat(neo-tree): Add keymap for creating new files in Neo-tree directory

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,5 +1,42 @@
 return {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
-    dependencies = {"nvim-lua/plenary.nvim", "MunifTanjim/nui.nvim", "nvim-tree/nvim-web-devicons"}
+    dependencies = {"nvim-lua/plenary.nvim", "MunifTanjim/nui.nvim", "nvim-tree/nvim-web-devicons"},
+    config = function()
+        local has, neotree = pcall(require, "neo-tree")
+        if not has then
+            return
+        end
+
+        neotree.setup({
+            close_if_last_window = false,
+            popup_border_style = "rounded",
+            enable_git_status = true,
+            enable_diagnostics = false,
+            default_component_configs = {
+                indent = { padding = 0 },
+            },
+            filesystem = {
+                follow_current_file = true,
+                use_libuv_file_watcher = true,
+                window = {
+                    mappings = {
+                        ["a"] = "add", -- create file
+                        ["A"] = "add_directory",
+                        ["d"] = "delete",
+                        ["r"] = "rename",
+                        ["y"] = "copy_to_clipboard",
+                        ["c"] = "copy",
+                        ["x"] = "cut_to_clipboard",
+                        ["p"] = "paste_from_clipboard",
+                        ["o"] = "open",
+                    },
+                },
+            },
+            window = {
+                position = "left",
+                width = 35,
+            },
+        })
+    end,
 }
