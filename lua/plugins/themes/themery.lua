31cfa8081b9293a5e90ba84c5b6962bd400b07b0
yashksaini-coder <yashksaini89@gmail.com>
2025-12-30 21:48:56 +0530

    refactor: remove oxocarbon theme plugin configuration

--- lua/plugins/themes/themery.lua
+++ lua/plugins/themes/themery.lua
@@ -4,15 +4,12 @@ return {
 	config = function()
 		require("themery").setup({
 			themes = {
-				"blue",
 				"tokyonight",
 				"tokyonight-storm",
 				"tokyonight-night",
 				"osmium",
 				"chai",
 				"gruvbox",
-				"gruvbuddy",
-				"oxocarbon",
 			},
 			livePreview = true, -- Apply theme while picking
 		})
