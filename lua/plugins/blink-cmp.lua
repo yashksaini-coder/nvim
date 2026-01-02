a1a3d610f03233b371f068c5b338517004d34bfb
yashksaini-coder <yashksaini89@gmail.com>
2026-01-02 16:58:36 +0530

    Add IDE-like keymaps for snippet expansion and completion in blink-cmp.lua
    Implemented key mappings for Tab, Shift-Tab, and Esc to enhance snippet management and completion behavior. Added functionality for navigating through snippets and completion items, while maintaining existing keymap consistency for a better user experience.

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -7,6 +7,8 @@ return {
 	},
 	event = "InsertEnter",
 	config = function()
+		local luasnip = require("luasnip")
+
 		-- Load friendly-snippets
 		require("luasnip.loaders.from_vscode").lazy_load()
 
@@ -46,6 +48,75 @@ return {
 				},
 			},
 		})
+
+		-- IDE-like keymaps for completion
+		-- Tab: Accept completion or expand/jump snippet
+		vim.keymap.set("i", "<Tab>", function()
+			-- Check if snippet can be expanded or jumped first
+			if luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
+			-- Check if completion menu is visible (using vim's pumvisible)
+			elseif vim.fn.pumvisible() == 1 then
+				-- Accept the currently selected completion
+				vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-y>", true, false, true), "n", false)
+			-- Otherwise, insert a tab
+			else
+				vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, false, true), "n", false)
+			end
+		end, { desc = "Accept completion or expand snippet" })
+
+		-- Shift-Tab: Previous completion item or previous snippet node
+		vim.keymap.set("i", "<S-Tab>", function()
+			-- Check if we can jump to previous snippet node
+			if luasnip.jumpable(-1) then
+				luasnip.jump(-1)
+			-- Check if completion menu is visible
+			elseif vim.fn.pumvisible() == 1 then
+				vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-p>", true, false, true), "n", false)
+			-- Otherwise, insert shift-tab
+			else
+				vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<S-Tab>", true, false, true), "n", false)
+			end
+		end, { desc = "Previous completion item or snippet node" })
+
+		-- Enter: Normal behavior (no special keymap)
+		-- Enter will work as normal newline, Tab is used to accept completion
+
+		-- Esc: Close completion menu
+		vim.keymap.set("i", "<Esc>", function()
+			-- If completion menu is visible, close it
+			if vim.fn.pumvisible() == 1 then
+				vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-e>", true, false, true), "n", false)
+			-- If snippet is active, exit snippet mode
+			elseif luasnip.choice_active() then
+				luasnip.unlink_current()
+			-- Otherwise, normal escape
+			else
+				vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "n", false)
+			end
+		end, { desc = "Close completion menu or exit snippet" })
+
+		-- Keep existing snippet keymaps for consistency
+		-- C-l: Expand or jump to next snippet node
+		vim.keymap.set({ "i", "s" }, "<C-l>", function()
+			if luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
+			end
+		end, { desc = "Expand or jump to next snippet node" })
+
+		-- C-h: Jump to previous snippet node
+		vim.keymap.set({ "i", "s" }, "<C-h>", function()
+			if luasnip.jumpable(-1) then
+				luasnip.jump(-1)
+			end
+		end, { desc = "Jump to previous snippet node" })
+
+		-- C-j: Change snippet choice
+		vim.keymap.set({ "i", "s" }, "<C-j>", function()
+			if luasnip.choice_active() then
+				luasnip.change_choice(1)
+			end
+		end, { desc = "Change snippet choice" })
 	end,
 }
 
