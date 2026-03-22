75a81aa5c1be1429022de8eb6b276dd4c1dd1ef6
yashksaini-coder <yashksaini89@gmail.com>
2026-03-22 11:52:06 +0530

    feat: auto-open nvim-tree when opening a directory

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -8,6 +8,17 @@ return {
 	dependencies = {
 		"nvim-tree/nvim-web-devicons",
 	},
+	init = function()
+		-- Auto-open nvim-tree when opening a directory (e.g. `nvim .`)
+		vim.api.nvim_create_autocmd("VimEnter", {
+			callback = function(data)
+				if vim.fn.isdirectory(data.file) == 1 then
+					vim.cmd.cd(data.file)
+					require("nvim-tree.api").tree.open()
+				end
+			end,
+		})
+	end,
 	config = function()
 		require("nvim-tree").setup({
 			sort = {
