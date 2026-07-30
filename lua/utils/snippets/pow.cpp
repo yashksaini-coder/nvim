2ea20e7c83c53cd6c7a3227379201876731f672d
Yash Kumar Saini <yashksaini89@gmail.com>
2026-07-30 13:45:47 +0530

    Fixed format and lint

--- lua/utils/snippets/pow.cpp
+++ lua/utils/snippets/pow.cpp
@@ -1,7 +1,9 @@
 int pow(int a, int b, int M) {
-	if (!b) return 1 % M;
-	long long ans = pow(a, b / 2, M);
-	ans = (ans * ans) % M;
-	if (b & 1) ans = (ans * a) % M;
-	return (int)ans;
+  if (!b)
+    return 1 % M;
+  long long ans = pow(a, b / 2, M);
+  ans = (ans * ans) % M;
+  if (b & 1)
+    ans = (ans * a) % M;
+  return (int)ans;
 }
